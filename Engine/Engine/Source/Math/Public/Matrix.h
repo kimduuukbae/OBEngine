45ab6e2c2c681d117601cf540906b5f8617fb88d
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2022-03-26 20:44:54 +0900

    update Matrix

--- Engine/Engine/Source/Math/Public/Matrix.h
+++ Engine/Engine/Source/Math/Public/Matrix.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "Vector4.h"
+
+struct Matrix {
+	alignas(16) float m[4][4];
+
+	Matrix();
+	Matrix(const Vector4& newX, const Vector4& newY, const Vector4& newZ, const Vector4& newW);
+	Matrix(const Vector3& newX, const Vector3& newY, const Vector3& newZ);
+
+	inline Matrix operator*(const Matrix& other) const;
+	inline Matrix operator+(const Matrix& other) const;
+	inline Matrix operator-(const Matrix& other) const;
+
+	inline Matrix operator==(const Matrix& other) const;
+	inline Matrix operator!=(const Matrix& other) const;
+
+	inline Matrix Inverse() const;
+
+	inline float GetDeterminant() const;
+	inline void Transpose();
+	inline Matrix GetTranspose();
+};
